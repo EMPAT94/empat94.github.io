556f48aaa0fb6f466de5b75491950c056af337f0
Pritesh Tupe <mail@priteshtupe.com>
2022-07-01 19:46:48 +0530

    Formatting and config updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,45 +1,45 @@
-const pluginRss = require('@11ty/eleventy-plugin-rss')
-const sitemap = require('@quasibit/eleventy-plugin-sitemap')
+const pluginRss = require("@11ty/eleventy-plugin-rss")
+const sitemap = require("@quasibit/eleventy-plugin-sitemap")
 
-const card = require('./src/_includes/components/card.js')
+const card = require("./src/_includes/components/card.js")
 
-module.exports = (eleventyConfig) => {
+module.exports = eleventyConfig => {
   // Do not use assets/, else css is included as well.
-  eleventyConfig.addPassthroughCopy('src/assets/icons/')
-  eleventyConfig.addPassthroughCopy('src/assets/imgs/')
-  eleventyConfig.addPassthroughCopy('src/assets/docs/')
-  eleventyConfig.addPassthroughCopy('src/assets/js/')
+  eleventyConfig.addPassthroughCopy("src/assets/icons/")
+  eleventyConfig.addPassthroughCopy("src/assets/imgs/")
+  eleventyConfig.addPassthroughCopy("src/assets/docs/")
+  eleventyConfig.addPassthroughCopy("src/assets/js/")
 
-  eleventyConfig.addPassthroughCopy('src/site.webmanifest')
-  eleventyConfig.addPassthroughCopy('src/robots.txt')
-  eleventyConfig.addPassthroughCopy('src/CNAME')
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest")
+  eleventyConfig.addPassthroughCopy("src/robots.txt")
+  eleventyConfig.addPassthroughCopy("src/CNAME")
 
-  eleventyConfig.addCollection('gs_sorted', function (collectionApi) {
+  eleventyConfig.addCollection("gs_sorted", function (collectionApi) {
     return collectionApi
-      .getFilteredByTag('gs')
+      .getFilteredByTag("gs")
       .sort((a, b) => a.data.part - b.data.part)
   })
 
-  eleventyConfig.addCollection('foa_sorted', function (collectionApi) {
+  eleventyConfig.addCollection("foa_sorted", function (collectionApi) {
     return collectionApi
-      .getFilteredByTag('foa')
+      .getFilteredByTag("foa")
       .sort((a, b) => a.data.part - b.data.part)
   })
 
-  eleventyConfig.addPairedShortcode('card', card)
+  eleventyConfig.addPairedShortcode("card", card)
 
   eleventyConfig.addPlugin(pluginRss)
 
   eleventyConfig.addPlugin(sitemap, {
     sitemap: {
-      hostname: 'https://priteshtupe.com'
-    }
+      hostname: "https://priteshtupe.com",
+    },
   })
 
   return {
     dir: {
-      input: 'src',
-      output: 'docs'
-    }
+      input: "src",
+      output: "docs",
+    },
   }
 }
