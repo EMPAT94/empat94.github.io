f7e486e3e3b7879310de5f0e8131ff4e296abad3
Pritesh Tupe <mail@priteshtupe.com>
2022-09-21 21:23:06 -0400

    Add lunr package for search functionality

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,22 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss")
 const sitemap = require("@quasibit/eleventy-plugin-sitemap")
+const lunr = require("lunr")
+
+const { write, unlink } = require("fs/promises")
 
 const card = require("./src/_includes/components/card.js")
+const { writeFile } = require("fs")
 
+let indexBuilt = false
 module.exports = eleventyConfig => {
-  // Do not use assets/, else css is included as well.
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest")
+  eleventyConfig.addPassthroughCopy("src/robots.txt")
+  eleventyConfig.addPassthroughCopy("src/CNAME")
+  // Do not use while assets/, else css is included as well.
   eleventyConfig.addPassthroughCopy("src/assets/icons/")
   eleventyConfig.addPassthroughCopy("src/assets/imgs/")
   eleventyConfig.addPassthroughCopy("src/assets/docs/")
   eleventyConfig.addPassthroughCopy("src/assets/js/")
-
-  eleventyConfig.addPassthroughCopy("src/site.webmanifest")
-  eleventyConfig.addPassthroughCopy("src/robots.txt")
-  eleventyConfig.addPassthroughCopy("src/CNAME")
 
   eleventyConfig.addCollection("gs_sorted", function (collectionApi) {
     return collectionApi
@@ -27,13 +31,37 @@ module.exports = eleventyConfig => {
   })
 
   eleventyConfig.addCollection("notes", function (collectionApi) {
-    return collectionApi
-      .getFilteredByTag("notes")
-      .map(n => {
-        n.title = n.url.split("/")[2]
-        return n
+    const all_notes = collectionApi.getFilteredByTag("notes").map(n => {
+      n.title = n.fileSlug
+      n.text = n.template.inputContent
+      return n
+    })
+
+    if (!indexBuilt) {
+      console.log("Building notes search index...")
+
+      const idx = lunr(function () {
+        this.ref("url")
+        this.field("text")
+        all_notes.forEach(this.add, this)
       })
-      .filter(n => !["README"].includes(n.title))
+
+      const data = [
+        "---\n",
+        "permalink: notesIndex.json\n",
+        "layout: ''\n",
+        "---\n",
+        JSON.stringify(idx),
+      ].join("")
+
+      const file = "./src/pages/notesIndex.json"
+
+      writeFile(file, data, err => err && console.error(err))
+
+      indexBuilt = true
+    }
+
+    return all_notes
   })
 
   eleventyConfig.addPairedShortcode("card", card)
