9f9e2c7d55f9b5568bc252057978db23dfe119f9
Pritesh Tupe <mail@priteshtupe.com>
2024-05-17 22:23:47 -0230

    Add new photo

--- src/_data/photos.js
+++ src/_data/photos.js
@@ -1,10 +1,19 @@
 module.exports = [
   {
+    src: "https://i.imgur.com/094QjfB.jpg",
+    location:
+      "Twillingate Lighthouse, Lighthouse Rd, NL-340, Crow Head, NL A0G 4M0",
+    title: "On the edge",
+    description: "View along the small trail",
+    datetime: "Fri May 12, 2024 6:48 pm EST",
+    tags: ["landscape", "sea"],
+  },
+  {
     src: "https://i.imgur.com/T22Gr0a.jpg",
-    location: "The Bagel Cafe | 246 Duckworth St, St. John's, NL A1C 1G8",
-    title: "Breakfast Bagel with a side of Fruit Bowl",
+    location: "The Bagel Cafe, 246 Duckworth St, St. John's, NL A1C 1G8",
+    title: "Breakfast Bagel with a side of Fruit bowl",
     description: "",
-    datetime: "May 12, 2024 9:09 am",
+    datetime: "Sun May 12, 2024 9:09 am EST",
     tags: ["food"],
   },
 ]
