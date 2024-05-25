9c5ca67594e08d6f2808fd66c4889540f1595b9c
Pritesh Tupe <mail@priteshtupe.com>
2024-05-25 16:28:41 -0300

    Add more photos, update photo page

--- src/_data/photos.js
+++ src/_data/photos.js
@@ -1,11 +1,28 @@
 module.exports = [
   {
+    src: "https://imgur.com/DweBLu9.jpg",
+    location: "8619 Cavendish Rd, Cavendish, PE C0A 1N0",
+    title: "Green Gables Heritage Place",
+    description: "From Anne of Green Gables",
+    datetime: "Sat May 25, 2024 10:41 am",
+    tags: ["heritage", "house", "landscape"],
+  },
+
+  {
+    src: "https://i.imgur.com/cAKYBCP.jpg",
+    location: "Twillingate, NL",
+    title: "Paddy",
+    description: "",
+    datetime: "Fri May 17, 2024 7:29 pm",
+    tags: ["animals", "cat"],
+  },
+  {
     src: "https://i.imgur.com/094QjfB.jpg",
     location:
       "Twillingate Lighthouse, Lighthouse Rd, NL-340, Crow Head, NL A0G 4M0",
     title: "On the edge",
     description: "View along the small trail",
-    datetime: "Fri May 12, 2024 6:48 pm EST",
+    datetime: "Fri May 12, 2024 6:48 pm",
     tags: ["landscape", "sea"],
   },
   {
@@ -13,7 +30,7 @@ module.exports = [
     location: "The Bagel Cafe, 246 Duckworth St, St. John's, NL A1C 1G8",
     title: "Breakfast Bagel with a side of Fruit bowl",
     description: "",
-    datetime: "Sun May 12, 2024 9:09 am EST",
+    datetime: "Sun May 12, 2024 9:09 am",
     tags: ["food"],
   },
 ]
