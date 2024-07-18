374f118133041593ccf8a9cd43a7d204a538758a
Pritesh Tupe <mail@priteshtupe.com>
2024-07-18 09:58:16 -0600

    Add image caching option

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,38 @@
+require("dotenv").config()
+const Image = require("@11ty/eleventy-img")
 const pluginRss = require("@11ty/eleventy-plugin-rss")
 const sitemap = require("@quasibit/eleventy-plugin-sitemap")
 const lunr = require("lunr")
 
 const card = require("./src/_includes/components/card.js")
-const { writeFile } = require("fs")
 
 let indexBuilt = false
 module.exports = eleventyConfig => {
-  eleventyConfig.addPassthroughCopy("src/site.webmanifest")
-  eleventyConfig.addPassthroughCopy("src/robots.txt")
-  eleventyConfig.addPassthroughCopy("src/CNAME")
-  // Do not use whole assets/, else css is included as well.
-  eleventyConfig.addPassthroughCopy("src/assets/icons/")
-  eleventyConfig.addPassthroughCopy("src/assets/imgs/")
-  eleventyConfig.addPassthroughCopy("src/assets/docs/")
-  eleventyConfig.addPassthroughCopy("src/assets/js/")
+  addCollections(eleventyConfig)
 
+  eleventyConfig.addPairedShortcode("card", card)
+
+  eleventyConfig.addPlugin(pluginRss)
+
+  eleventyConfig.addPlugin(sitemap, {
+    sitemap: {
+      hostname: "https://priteshtupe.com",
+    },
+  })
+
+  addImageShortCode(eleventyConfig)
+
+  addPassthroughCopy(eleventyConfig)
+
+  return {
+    dir: {
+      input: "src",
+      output: "docs",
+    },
+  }
+}
+
+function addCollections(eleventyConfig) {
   eleventyConfig.addCollection("gs_sorted", function (collectionApi) {
     return collectionApi
       .getFilteredByTag("gs")
@@ -36,46 +53,71 @@ module.exports = eleventyConfig => {
     })
 
     if (!indexBuilt) {
-      console.log("Building notes search index...")
-
-      const idx = lunr(function () {
-        this.ref("url")
-        this.field("text")
-        all_notes.forEach(this.add, this)
-      })
-
-      const data = [
-        "---\n",
-        "permalink: notesIndex.json\n",
-        "layout: ''\n",
-        "---\n",
-        JSON.stringify(idx),
-      ].join("")
-
-      const file = "./src/pages/notesIndex.html"
-
-      writeFile(file, data, err => err && console.error(err))
-
-      indexBuilt = true
+      buildNotesIndex(all_notes)
     }
 
     return all_notes
   })
+}
 
-  eleventyConfig.addPairedShortcode("card", card)
+function buildNotesIndex(all_notes) {
+  console.log("Building notes search index...")
 
-  eleventyConfig.addPlugin(pluginRss)
-
-  eleventyConfig.addPlugin(sitemap, {
-    sitemap: {
-      hostname: "https://priteshtupe.com",
-    },
+  const idx = lunr(function () {
+    this.ref("url")
+    this.field("text")
+    all_notes.forEach(this.add, this)
   })
 
-  return {
-    dir: {
-      input: "src",
-      output: "docs",
+  const data = [
+    "---\n",
+    "permalink: notesIndex.json\n",
+    "layout: ''\n",
+    "---\n",
+    JSON.stringify(idx),
+  ].join("")
+
+  const file = "./src/pages/notesIndex.html"
+
+  require("fs").writeFile(file, data, err => err && console.error(err))
+
+  indexBuilt = true
+}
+
+function addImageShortCode(eleventyConfig) {
+  console.log("Caching images...")
+
+  const IMAGE_URL = process.env.PERSONAL_WEBSITE_MEDIA
+
+  eleventyConfig.addShortcode(
+    "image",
+    async function (src, alt, className, onClick) {
+      let metadata = await Image(IMAGE_URL + src, {
+        formats: ["webp"],
+        outputDir: "./docs/assets/imgs",
+        urlPath: "/assets/imgs",
+      })
+
+      let imageAttributes = {
+        alt,
+        onClick,
+        class: className,
+        loading: "lazy",
+        decoding: "async",
+      }
+
+      return Image.generateHTML(metadata, imageAttributes)
     },
-  }
+  )
+}
+
+function addPassthroughCopy(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest")
+  eleventyConfig.addPassthroughCopy("src/robots.txt")
+  eleventyConfig.addPassthroughCopy("src/CNAME")
+  // Do not use whole assets/, else css is included as well.
+  eleventyConfig.addPassthroughCopy("src/assets/icons/")
+  eleventyConfig.addPassthroughCopy("src/assets/imgs/")
+  eleventyConfig.addPassthroughCopy("src/assets/docs/")
+  eleventyConfig.addPassthroughCopy("src/assets/js/")
 }
